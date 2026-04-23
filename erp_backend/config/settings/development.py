bc60b12be951c61d507757cfcb0f8286e4e2fda4
Denish <denishkumbharvadiya.cse23@adaniuni.ac.in>
2026-04-24 02:39:53 +0530

    fix: allow all hosts in development settings to unblock Vercel endpoint

--- erp_backend/config/settings/development.py
+++ erp_backend/config/settings/development.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'django-insecure-dev-key-change-in-production-abc123xyz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Database
 DATABASES = {
